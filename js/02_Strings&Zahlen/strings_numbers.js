9413aa03671504caa424980c7e0184d6ee38e319
botrick01 <johannsenpatrick.privat@gmail.com>
2023-03-10 14:38:13 +0100

    strings and numbers

---
+++ js/02_Strings&Zahlen/strings_numbers.js
@@ -0,0 +1,9 @@
+//	1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461538461538462
+//2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+//"Ich heiße VORNAME NACHNAME"
+let a = 17.4 / 3.9;
+console.log("The result of 17.4 divided by 3.9 is " + a);
+let firstname = "Patrick";
+let lastname = "Johannsen";
+console.log("Ich heiße " + firstname + " " + lastname);
